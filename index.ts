538265abf68cf1a6f7423680a460997dffd0a646
Maz <mazspm@icloud.com>
2024-04-09 18:22:39 -0500

    server is working

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/todo/:id", (context) => {
+  const todoId = Number(context.req.param("id"));
   return context.json({ message: "hello" });
 });
 
